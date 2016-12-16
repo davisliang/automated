2722678509e7ed87133aa74c6708be5dc2cfb774
davisliang <Davis@daviss-mbp.ucsd.edu>
2016-12-16 12:29:54 -0800

    added python script to remove any ascii characters not in columns 2 and 4. Added script to visualize preprocessed tweets.

---
+++ visualizeData.py
@@ -0,0 +1,13 @@
+import cPickle as pickle
+import numpy as np
+
+tweets = pickle.load(open("preprocessed_new_tweets","rb"))
+embeddings = pickle.load(open("new_embeddings.pkl","rb"))
+
+print "tweet array shape: ", len(tweets)
+print "embeddings array shape: ", embeddings.shape
+print "tweet array type: ", type(tweets[0])
+print "embeddings array type: ", type(embeddings[0])
+
+for i in range(100):
+	print tweets[i]
