f7c6b36331adf5639e78afb4cc28306c767d96ce
davisliang <Davis@daviss-mbp.ucsd.edu>
2016-12-16 14:03:28 -0800

    formatting+2

--- TwitterStreamSpout.java
+++ TwitterStreamSpout.java
@@ -40,7 +40,7 @@ public class TwitterStreamSpout extends BaseRichSpout {
     String accessToken;
     String accessTokenSecret;
     String[] keyWords;
-		
+        
     public TwitterStreamSpout(String consumerKey, String consumerSecret,
         String accessToken, String accessTokenSecret, String[] keyWords) {
 
@@ -49,94 +49,94 @@ public class TwitterStreamSpout extends BaseRichSpout {
         this.accessToken = accessToken;
         this.accessTokenSecret = accessTokenSecret;
         this.keyWords = keyWords;
-	}
-		
-	public TwitterStreamSpout() {
-	  // TODO Auto-generated constructor stub
-	}
-		
-	@Override
-	public void open(Map conf, TopologyContext context,
-      	SpoutOutputCollector collector) {
+    }
+        
+    public TwitterStreamSpout() {
+      // TODO Auto-generated constructor stub
+    }
+        
+    @Override
+    public void open(Map conf, TopologyContext context,
+        SpoutOutputCollector collector) {
 
-		queue = new LinkedBlockingQueue<Status>(1000);
-		_collector = collector;
+        queue = new LinkedBlockingQueue<Status>(1000);
+        _collector = collector;
 
-		StatusListener listener = new StatusListener() {
+        StatusListener listener = new StatusListener() {
 
         @Override
         public void onStatus(Status status) {
            queue.offer(status);
         }
-				
+                
         @Override
         public void onDeletionNotice(StatusDeletionNotice sdn) {}
-				
+                
         @Override
         public void onTrackLimitationNotice(int i) {}
-				
+                
         @Override
         public void onScrubGeo(long l, long l1) {}
-				
+                
         @Override
         public void onException(Exception ex) {}
-				
+                
         @Override
         public void onStallWarning(StallWarning arg0) {
            // TODO Auto-generated method stub
         }
     };
-				
-		ConfigurationBuilder cb = new ConfigurationBuilder(); 
-			
-		cb.setDebugEnabled(true)
-		.setOAuthConsumerKey(consumerKey)
-		.setOAuthConsumerSecret(consumerSecret)
-		.setOAuthAccessToken(accessToken)
-		.setOAuthAccessTokenSecret(accessTokenSecret);
-				
-		_twitterStream = new TwitterStreamFactory(cb.build()).getInstance();
-		_twitterStream.addListener(listener);
-			
-		if (keyWords.length == 0) {
-			_twitterStream.sample();
-		}else {
-			FilterQuery query = new FilterQuery().track(keyWords);
-			_twitterStream.filter(query);
-		}
+                
+        ConfigurationBuilder cb = new ConfigurationBuilder(); 
+            
+        cb.setDebugEnabled(true)
+        .setOAuthConsumerKey(consumerKey)
+        .setOAuthConsumerSecret(consumerSecret)
+        .setOAuthAccessToken(accessToken)
+        .setOAuthAccessTokenSecret(accessTokenSecret);
+                
+        _twitterStream = new TwitterStreamFactory(cb.build()).getInstance();
+        _twitterStream.addListener(listener);
+            
+        if (keyWords.length == 0) {
+            _twitterStream.sample();
+        }else {
+            FilterQuery query = new FilterQuery().track(keyWords);
+            _twitterStream.filter(query);
+        }
     }
-			
+            
     @Override
     public void nextTuple() {
-      	Status ret = queue.poll();
-				
-		if (ret == null) {
-			Utils.sleep(50);
-		} else {
-			_collector.emit(new Values(ret));
-		}
+        Status ret = queue.poll();
+                
+        if (ret == null) {
+            Utils.sleep(50);
+        } else {
+            _collector.emit(new Values(ret));
+        }
     }
-			
+            
     @Override
     public void close() {
-		_twitterStream.shutdown();
+        _twitterStream.shutdown();
     }
-			
-	@Override
-	public Map<String, Object> getComponentConfiguration() {
-		Config ret = new Config();
-		ret.setMaxTaskParallelism(1);
-		return ret;
-	}
-			
-	@Override
-	public void ack(Object id) {}
-			
-	@Override
-	public void fail(Object id) {}
-			
-	@Override
-	public void declareOutputFields(OutputFieldsDeclarer declarer) {
-		declarer.declare(new Fields("tweet"));
-	}
+            
+    @Override
+    public Map<String, Object> getComponentConfiguration() {
+        Config ret = new Config();
+        ret.setMaxTaskParallelism(1);
+        return ret;
+    }
+            
+    @Override
+    public void ack(Object id) {}
+            
+    @Override
+    public void fail(Object id) {}
+            
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        declarer.declare(new Fields("tweet"));
+    }
 }
