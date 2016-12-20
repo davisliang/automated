e68f5b11c128f1db39efc726966424309dc35f5c
Davis Liang <davisblaine.liang@gmail.com>
2016-12-19 21:36:00 -0800

    successfully loaded variable size dataset to 3 dimensional numpy array. Code is tested and working.

--- tweetnet.py
+++ tweetnet.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cPickle as pickle
-
+from ReducedAsciiDictionary import ReducedAsciiDictionary
 '''
 from keras.models import Sequential
 from keras.layers import Dense
@@ -15,17 +15,41 @@ tweets = pickle.load(open("preprocessed_new_tweets.pkl","rb"))
 embeddings = pickle.load(open("new_embeddings.pkl","rb"))
 
 #visualize data
-print "tweets (ELEMENT TYPE):", type(tweets[0])
-print "tweets (SHAPE): ", len(tweets)
+print "tweets (ELEMENT TYPE): ", type(tweets[0])
+print "tweets (Number Of Tweets): ", len(tweets)
 
-print "hashtag (ELEMENT TYPE):", type(embeddings[0])
+print "hashtag (ELEMENT TYPE): ", type(embeddings[0])
 print "hashtag (SHAPE): ", embeddings.shape
 
-'''
-tweets (ELEMENT TYPE): <type 'str'>
-tweets (SHAPE):  34714
-hashtag (ELEMENT TYPE): <type 'numpy.ndarray'>
-hashtag (SHAPE):  (34714, 300)
-'''
-
 #create character dictionary for tweets.
+dictionary = ReducedAsciiDictionary({},np.array([])).dictionary
+
+
+#load first 1000 tweets
+#recall numpy array should be of shape (samples, time steps, features)
+numData = 1000
+timeSteps = 10
+X = np.zeros([numData, 140+1, len(dictionary) + embeddings.shape[1]])
+
+# for each tweet
+for twt in range(numData):
+	currTweet = tweets[twt][6:len(tweets[twt])]
+	for ch in range(len(currTweet)):
+	    oneHotIndex = dictionary.get(currTweet[ch])
+	    X[twt,ch,oneHotIndex] = 1
+		
+	    for embIndex in range(embeddings.shape[1]):
+			X[twt,ch,embIndex+len(dictionary)] = embeddings[twt,embIndex]
+	#end of tweet indentifier is when first and second are ones
+	X[twt,len(currTweet),0]=-1
+ 
+print "data (TYPE): ", type(X)
+print "data (SHAPE): ", X.shape
+
+print "first tweet, first character: ", tweets[0][6]
+print "first tweet, first character index: ", dictionary.get(tweets[0][6])
+print "first tweet, first character vector: ", X[0,0]
+
+print "random tweet, random character: ", tweets[253][12+6]
+print "random tweet, random cahracter index: ", dictionary.get(tweets[253][12+6])
+print "random tweet, random character vector:", X[253,12]
