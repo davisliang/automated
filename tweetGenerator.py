52ebd969f0163992a0868920ba776dc39e33209c
davisliang <Davis@Daviss-MacBook-Pro.local>
2017-01-06 15:20:19 -0800

    slightly optimized text generation code. TO-DO:  Restyling required. Unit testing required.

--- tweetGenerator.py
+++ tweetGenerator.py
@@ -1,4 +1,7 @@
 '''
+This code should not be used. Mega inefficient and a waste of RAM.
+Will fix soon... :). Code calling this method from other classes
+Have been commented out.
 Reads weight matrix from hdf5 file and generates text using seed.
 '''
 import numpy as np
@@ -15,42 +18,17 @@ from keras.layers import Dropout
 from keras.layers import BatchNormalization
 
 
-def generateText(dictionary):
+def generateText(dictionary, data, dictLen, tweetLen, X, y, 
+	inputSize, sequenceLength, numHiddenFirst, numTweets, seqPerSegment,
+	n_examples, numSegments):
 
 	print("Start loading data ...")
-	data, dictLen, tweetLen, dictionary = loadData({},np.array([])) 
+
 	# data shape = #tweets x 141 x inputSize(365)
 	#initialize inverse dictionary to map integers to characterse
 	inverseDictionary = {v: k for k, v in dictionary.iteritems()}
 	print("Finished loading data")
 
-	#initialize some hyper-parameters
-	#inputSize: size of each input vector (default: 365x1)
-	inputSize = data.shape[2]
-	#sequenceLength: sequence length (k in BPTTk)
-	sequenceLength = 50
-	#numHiddenFirst: size of first hidden layer
-	numHiddenFirst = 512
-	#numTweets: total number of tweets in dataset
-	numTweets = data.shape[0]
-	#seqPerSegment: sequences (of size sequenceLength) per mini-epoch. 
-	#Lowers maximum memory usage.
-	seqPerSegment = 10000
-
-	X = []
-	y = []
-
-	#create input and target datasets from loaded data.
-	for i in range(numTweets):
-	    for j in range(0, int(tweetLen[i])-sequenceLength, 1):
-	        seq_in = data[i, j:j+sequenceLength, :]
-	        seq_out = data[i, j+sequenceLength, 0:dictLen]
-	        X.append(seq_in)
-	        y.append(seq_out)
-
-	#X: [10000 (numTweets), 40 (sequenceLength), 365(inputSize)].
-	n_examples = len(X)
-	numSegments = np.ceil(n_examples/seqPerSegment).astype(int)
 	print('# of sequences per segments: ', seqPerSegment)
 	print('# of segments: ', numSegments)
 
