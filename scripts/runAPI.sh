330f56cc10f3f69a8284b2b1eacd318778ff2feb
davisliang <Davis@Daviss-MacBook-Pro.local>
2017-01-23 20:37:45 -0800

    updated some bugs that occured when full path names changed. Notably: used the System function in java to grab user.home because java does not recognize '~'.

--- scripts/runAPI.sh
+++ scripts/runAPI.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "Starting up TweetFeeder."
 
-STORMPATH="~/apache-storm-0.9.5/lib/*"
-TWITTERPATH="~/twitter4j-4.0.4/lib/*"
-CLASSPATH="~/tweetnet"
+STORMPATH="/Users/Davis/apache-storm-0.9.5/lib/*"
+TWITTERPATH="/Users/Davis/twitter4j-4.0.4/lib/*"
+CLASSPATH="/Users/Davis/tweetnet/src/"
 
 javac -cp $STORMPATH:$TWITTERPATH ~/tweetnet/src/TwitterStreamSpout.java ~/tweetnet/src/TwitterCleanerBolt.java ~/tweetnet/src/TwitterStorm.java
 
-java -cp $STORMPATH:$TWITTERPATH:. ~/tweetnet/src/TwitterStorm mneD3kPggHIGMF3uzGrOYP6YT vb66pHEGPRbadl1fpwN4Um8yO4or46IigKVnYrvfP1obPLMZ4k  306922539-8OcnG1IASwyAjceFWekGt4quMrGDcxNmMtcmJ7ON  RJtwaYKfHvKkeI8YZdPVZ4zJLn9FCkZ3OK6aPkCiteQPC 
+java -cp $STORMPATH:$TWITTERPATH:$CLASSPATH TwitterStorm mneD3kPggHIGMF3uzGrOYP6YT vb66pHEGPRbadl1fpwN4Um8yO4or46IigKVnYrvfP1obPLMZ4k  306922539-8OcnG1IASwyAjceFWekGt4quMrGDcxNmMtcmJ7ON  RJtwaYKfHvKkeI8YZdPVZ4zJLn9FCkZ3OK6aPkCiteQPC 
