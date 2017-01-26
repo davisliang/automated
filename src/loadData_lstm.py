4ab9b2a81a3cb5f8023508b2ae0806ec8e78e09a
vasumv <vasumvikram@gmail.com>
2017-01-26 15:57:01 -0800

    Fixed several issues from review

--- src/loadData_lstm.py
+++ src/loadData_lstm.py
@@ -7,11 +7,13 @@ from ReducedAsciiDictionary import ReducedAsciiDictionary
 from os.path import expanduser
 import sys
 
-def loadData(dictionary,ranges,sequenceLength):
+def loadData(dictionary,ranges,sequenceLength, nTweet):
 	''' Creates dataset based on dictionary, a set of ascii
 	ranges, and pickled twitter data from Apache Storm.
 
-	X: (numTweets, 141, dictionaryLength + embeddings length)
+
+        X: [#sequences, 40, 65]
+        y: [#sequences, 65]
 	vocabLen: (dictionary length)
 	tweetLength: (numTweets)
 	'''
@@ -19,8 +21,11 @@ def loadData(dictionary,ranges,sequenceLength):
 	#load tweets and hashtag embeddings
 	tweets = pickle.load(open(expanduser("~/tweetnet/data/preprocessed_new_tweets.pkl"),"rb"))
         np.random.shuffle(tweets)
-        tweets = tweets[0:7000]
+
+        #use the first nTweet tweets
+        tweets = tweets[0:nTweet]
         print "Number of tweets ", len(tweets)
+
 	#create character dictionary for tweets.
 	dictionary = ReducedAsciiDictionary({},ranges).dictionary
 
@@ -42,10 +47,12 @@ def loadData(dictionary,ranges,sequenceLength):
                 tweetSequence.append(oneTweet[j : j+sequenceLength])
                 nextChar.append(oneTweet[j+sequenceLength])
             tweetSequence.append(oneTweet[len(oneTweet)-sequenceLength - 1:len(oneTweet) - 1])
-            nextChar.append("EOS")
+            nextChar.append("<EOS>")
         print('Number of sequences: ', len(tweetSequence))
 
 	# for each sequence, create onehot encoding for each character
+        # X: [#sequences, 40, 65]
+        # y: [#sequences, 65]
 	print("Vectorization...")
         X = np.zeros((len(tweetSequence), sequenceLength, vocabLen), dtype=np.bool)
         y = np.zeros((len(tweetSequence), vocabLen), dtype=np.bool)
@@ -57,12 +64,12 @@ def loadData(dictionary,ranges,sequenceLength):
 		oneHotIndex = dictionary.get(ch)
 		X[i,j,oneHotIndex] = 1
 	    
-            if len(nextChar[i]) == 1:
+            if nextChar[i] != "<EOS>":
                 y[i, dictionary.get(nextChar[i])] = 1
             else:
                 y[i, len(dictionary)] = 1
         return X, y, vocabLen, dictionary, tweetSequence, nextChar, tweets
 
 if __name__ == "__main__":
-    a,b,c,d,e,f = loadData({},np.array([]),40)
-    print e[0]
+    X, y, vocabLen, dictionary, tweetSequence, nextChar, tweets = loadData({},np.array([]),40)
+    print "The first tweet sequence is: ", tweetSequence[0]
--- src/tweetGenerator_lstm.py
+++ src/tweetGenerator_lstm.py
@@ -29,27 +29,36 @@ def sample(preds, temperature=1.0):
     return np.argmax(probas)
 
 def generateText(model, tweets, sequenceLength, vocabLen, dictionary):
+    
+    # Random select a tweet for generation
     start_index = random.randint(len(tweets))
     inverseDictionary = {v: k for k, v in dictionary.iteritems()}
     
+    # Different temperature adds randomness to character generation
     for diversity in [0.2, 0.5, 1.0, 1.2]:
         print("\n")
         print('----- diversity:', diversity)
 
         generated = ""
+
         seed = tweets[start_index][6:sequenceLength+6]
         print('----- Generating with seed: "' + seed + '"')
         generated += seed
         sys.stdout.write(generated)
 
         for i in range(140):
+
+            # x: [1, sequenceLength(40), 65]
             x = np.zeros((1, sequenceLength, vocabLen))
+            
+            # Create one hot encoding vectors for the seed
             for j, ch in enumerate(seed):
                 x[0, j, dictionary.get(ch)] = 1
 
             preds = model.predict(x, verbose=0)[0]
             next_index = sample(preds, diversity)
             
+            # If an EOS symbol is genearted, append "<EOS>" to the end of generated and stop
             if next_index == vocabLen - 1:
                 next_char = "<EOS>"
                 generated += next_char
@@ -58,9 +67,11 @@ def generateText(model, tweets, sequenceLength, vocabLen, dictionary):
                 sys.stdout.flush()
                 break
             
+            # If not an EOS symbol, append the last generated char
             else:
                 next_char = inverseDictionary[next_index]
                 generated += next_char
+                # Shift the window by 1 go create the new seed
                 seed = seed[1:] + next_char
                 sys.stdout.write(next_char)
                 sys.stdout.flush()
